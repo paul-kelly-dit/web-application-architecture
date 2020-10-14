5247b62e32a2e027821dd8f9a8f2aacd10eb4f53
Affan Ahmad <anonymous-ME@users.noreply.github.com>
2020-10-14 12:11:25 +0100

    Update jobapplication.test.js

--- week3/tdd-assignment/jobapplication.test.js
+++ week3/tdd-assignment/jobapplication.test.js
@@ -7,9 +7,9 @@ let applicantA = {
 }
 
 let applicantB = {
-    experienceInYears: 2,
+    experienceInYears: 3,
     industry: "hospitality",
-    bachelorsDegree: true
+    bachelorsDegree: false
 }
 
 let applicantC = {
