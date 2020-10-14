2081bde76ed5e4857ea7b6edaa7f0bc4af42e25d
Paul Kelly <pkelly03@gmail.com>
2020-10-14 07:02:32 +0100

    cookies example

---
+++ week9-security/cookies/README.md
@@ -0,0 +1,19 @@
+# Cookies example
+
+![](https://www.publicdomainpictures.net/pictures/10000/velka/33-1210498515t3lN.jpg)
+
+Follow the common [installation instructions](https://github.com/odino/wasec#installation). Then, run this example with nodejs, through `node index.js`.
+
+You can then visit:
+
+* [wasec.local, which sets a host-only cookie](http://wasec.local:7888/)
+* [the subdomain sub.wasec.local, which doesn't have access to the host-only cookie](http://sub.wasec.local:7888/)
+* [wasec.local?domain=on, which sets a cookie on the wasec.local domain and its subdomains](http://wasec.local:7888/?domain=on)
+* [sub.wasec.local again, which has access to the Domain cookie](http://sub.wasec.local:7888/)
+* [wasec.local?super=on, which tries to set a supercookie on the .local tld, but fails](http://wasec.local:7888/?super=on)
+* [wasec.local?clear=on if you want to clear cookies](http://wasec.local:7888/?clear=on)
+* [you can set a secure cookie at wasec.local?secure=on](https://wasec.local:7889/?secure=on), and verify that it is [not available when accessing the app via HTTP](http://wasec.local:7888/)
+* [check that HttpOnly cookies are not available to JavaScript](http://wasec.local:7888/?httponly=on): open the devtools, you will see an additional cookie that's not printed on the webpage
+* [try setting a SameSite cookie](http://wasec.local:7888/?samesite=on): then try executing a [cross-site request forgery](http://wasec2.local:7888/same-site-form)
+and see the cookie is not sent along the request. If you instead navigate to [wasec.local:7888](http://wasec.local:7888) yourself you will see the cookie
+sent across with the request
---
+++ week9-security/cookies/index.js
@@ -0,0 +1,93 @@
+var qs = require('querystring')
+var url = require('url')
+var fs = require('fs')
+
+function server (req, res) {
+  if (req.url === '/same-site-form') {
+    return res.end(`
+<html>
+  <form action="http://wasec.local:7888/" method="POST">
+    <input type="hidden" name="destination" value="attacker@email.com" />
+    <input type="hidden" name="amount" value="1000" />
+    <input type="submit" value="CLICK HERE TO WIN A HUMMER" />
+  </form>
+</html>
+`)
+  }
+  let query = qs.parse(url.parse(req.url).query)
+
+  if (query.clear === "on") {
+    res.writeHead('302', {
+      'Set-Cookie': [
+        'example=a;Expires=Wed, 21 Oct 2015 07:28:00 GMT',
+        `example_with_domain=a;Domain=wasec.local;Expires=Wed, 21 Oct 2015 07:28:00 GMT`,
+        `supercookie=a;Expires=Wed, 21 Oct 2015 07:28:00 GMT`,
+        `secure=a;Expires=Wed, 21 Oct 2015 07:28:00 GMT`,
+        `not_secure=a;Expires=Wed, 21 Oct 2015 07:28:00 GMT`,
+      ],
+      'Location': '/'
+    })
+    res.end()
+    return
+  }
+
+  let headers = {}
+  headers['Set-Cookie'] = []
+
+  if (!req.headers.host.startsWith('sub.wasec.local')) {
+    headers['Set-Cookie'].push('example=test')
+
+    if (query.domain === 'on') {
+      headers['Set-Cookie'].push(`example_with_domain=test_domain_cookie;Domain=wasec.local`)
+    }
+  }
+
+  if (query.super === 'on') {
+    headers['Set-Cookie'].push(`supercookie=test;Domain=local`)
+  }
+
+  if (query.secure === 'on') {
+    headers['Set-Cookie'].push(`secure=test;Secure`)
+  }
+
+  if (query.secure === 'on') {
+    headers['Set-Cookie'].push(`not_secure=test`)
+  }
+
+  if (query.httponly === 'on') {
+    headers['Set-Cookie'].push(`http_only_cookie=test;HttpOnly`)
+  }
+
+  if (query.samesite === 'on') {
+    headers['Set-Cookie'].push(`same_site_cookie=test;SameSite=Lax`)
+  }
+
+  res.writeHead(200, headers)
+  res.end(`
+<html>
+  <div id="output"/ >
+  <script>
+    let content = "none";
+
+    if (document.cookie) {
+      let cookies = document.cookie.split(';')
+      content = ''
+
+      cookies.forEach(c => {
+        content += "<p><code>" + c + "</code></p>"
+      })
+    }
+
+    document.getElementById('output').innerHTML = "Cookies on this document: <div>" + content + "</div>"
+  </script>
+<html>
+`)
+}
+
+const options = {
+  key: fs.readFileSync(__dirname + '/../wasec.local-key.pem'),
+  cert: fs.readFileSync(__dirname + '/../wasec.local.pem')
+};
+
+require('http').createServer(server).listen(7888)
+require('https').createServer(options, server).listen(7889)
