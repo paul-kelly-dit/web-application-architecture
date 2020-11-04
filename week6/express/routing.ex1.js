58c33ec2fdccc4386481e85c12c69b0f54db28df
Paul Kelly <pkelly03@gmail.com>
2020-11-04 05:54:30 +0000

    week6 - some additional examples

--- week6/express/routing.ex1.js
+++ week6/express/routing.ex1.js
@@ -1,5 +1,11 @@
+var express = require("express");
+var http = require("http");
+
 const app = express();
 
 app.get('/hello', (req, res) => {
+    res.status(200);
     res.send('Hello World');
 });
+
+http.createServer(app).listen(3000);
