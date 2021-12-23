7206442e33d57498a82fae5f83bab8633de86a2c
Paul Kelly <pakelly@shutterstock.com>
2021-12-23 11:07:44 +0000

    added action-creators

---
+++ react/action-creators/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createStore } from 'redux';
+
+const initialState = 0;
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PLUS': {
+      return state + (action.payload || 0);
+    }
+    case 'MINUS': {
+      return state - (action.payload || 0);
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const add = (payload) => ({ type: 'PLUS', payload });
+const subtract = (payload) => ({ type: 'MINUS', payload });
+
+const store = createStore(counterReducer);
+
+const initialstate = store.getState();
+
+store.dispatch(add(2));
+const dispatch_1 = store.getState();
+
+store.dispatch(add(1));
+const dispatch_2 = store.getState();
+
+store.dispatch(subtract(2));
+const dispatch_3 = store.getState();
+
+const App = () => {
+  return (
+    <div>
+      <p> Initial state: { initialstate } </p>
+      <p> State after +2 payload: { dispatch_1 } </p>
+      <p> State after +1 payload: { dispatch_2 } </p>
+      <p> State after -2 payload: { dispatch_3 } </p>
+    </div>
+  );
+};
+
+export default App;
